e6b3a097e774503c3148189b7d50aa9401ffa19a
Memo10100 <juaniiiii1025@gmail.com>
2023-04-12 03:19:30 -0300

    Primer Commit

---
+++ product-service/src/test/java/academy/digitallab/store/product/ServiceProductApplicationTests.java
@@ -0,0 +1,12 @@
+package academy.digitallab.store.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
